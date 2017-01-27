b6ee0d837d7bbefcebad94f1a83e87698730b5d9
DeathCon4.2 <klrehm123@gmail.com>
2017-01-26 19:02:11 -0500

    rocket fixes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("breakfast 2.0")]
+[assembly: AssemblyTitle("BreakfastAdminAbuse")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("breakfast 2.0")]
+[assembly: AssemblyProduct("BreakfastAdminAbuse")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
